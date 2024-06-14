8510aa6c3d129d8a71ca35c4da3831cf2c8b6871
Aniket lodh <63922607+Aniket-lodh@users.noreply.github.com>
2024-06-14 18:38:00 +0530

    Update client.js

--- src/utils/client.js
+++ src/utils/client.js
@@ -2,11 +2,11 @@ import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = sanityClient({
-  projectId: `${import.meta.env.VITE_SERVER_PROJECT_ID}`,
+  projectId: `${import.meta.env.SERVER_PROJECT_ID}`,
   dataset: "production",
   apiVersion: "2023-01-17",
   useCdn: false, //Make it true if you want to get cached fast responses
-  token: `${import.meta.env.VITE_SERVER_TOKEN}`,
+  token: `${import.meta.env.SERVER_TOKEN}`,
 });
 
 const builder = imageUrlBuilder(client);
